7fea371357f23186d11378b63f82b10d150701e3
Abid <abidmhsn@gmail.com>
2023-12-04 21:39:19 +0800

    Fix

--- src/components/Elements/HistoryBox.jsx
+++ src/components/Elements/HistoryBox.jsx
@@ -1,6 +1,6 @@
 const HistoryBox = () => {
     return (
-        <div className="flex gap-5">
+        <div className="flex gap-5 flex-wrap">
             <img src="https://i.ibb.co/BwNSbMb/logo-mamo.jpg" alt="" className="w-69 rounded-2xl drop-shadow-xl" />
             <div className="flex flex-col gap-5">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores fugit omnis cupiditate porro repellendus sint. Laborum explicabo sunt ducimus, deserunt fugit ea a molestiae, facilis consequuntur ad, aperiam aspernatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis autem nam temporibus consequatur earum est magnam ad laborum fuga quis, eaque nemo recusandae! Sint voluptatem at pariatur dolores rem quas?</p>
--- src/components/Elements/UmkmSummary.jsx
+++ src/components/Elements/UmkmSummary.jsx
@@ -1,11 +1,11 @@
 const UmkmSummary = () => {
     return (
         <div className="bg-white py-4 xl:px-0 p-4">
-            <div className="max-w-[1240px] mx-auto flex gap-4 ">
-                <img src="https://i.ibb.co/BwNSbMb/logo-mamo.jpg" alt="" className="w-[130px] rounded-full" />
+            <div className="max-w-[1240px] mx-auto flex gap-4 flex-wrap ">
+                <img src="https://i.ibb.co/BwNSbMb/logo-mamo.jpg" alt="" className="w-[130px] h-[130px] rounded-full" />
                 <div>
                     <h1 className="font-h1 text-2xl font-inter">Sambal Mamo</h1>
-                    <p className="font-inter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate! Numquam natus accusantium velit iusto sapiente quidem ullam pariatur ipsam tenetur tempore, distinctio facere at vitae ducimus, cupiditate saepe quia? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt hic corporis voluptates perspiciatis consequuntur soluta quaerat, quos non distinctio provident officiis porro magnam, praesentium velit beatae animi labore ad.</p>
+                    <p className="font-inter text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate! Numquam natus accusantium velit iusto sapiente quidem ullam pariatur ipsam tenetur tempore, distinctio facere at vitae ducimus, cupiditate saepe quia? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt hic corporis voluptates perspiciatis consequuntur soluta quaerat, quos non distinctio provident officiis porro magnam, praesentium velit beatae animi labore ad.</p>
                 </div>
             </div>
         </div>
