2fd45a51f7564d35b96a28206fd04ff18e815955
Abid <abidmhsn@gmail.com>
2023-12-04 21:03:04 +0800

    Tambah History

---
+++ src/components/Section/History.jsx
@@ -0,0 +1,14 @@
+import HistoryBox from "../Elements/HistoryBox";
+import Section from "./Index";
+
+const History = () => {
+    return (
+        <Section title="Sejarah">
+            <div className="mt-5">
+                <HistoryBox />
+            </div>
+        </Section>
+    )
+}
+
+export default History;
