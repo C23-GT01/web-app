e9d50197afd5a4fe5a215369b736d576d66241fb
Abid <abidmhsn@gmail.com>
2023-12-04 21:01:04 +0800

    Tambah UMKM image dan Summary

---
+++ src/components/Elements/UmkmSummary.jsx
@@ -0,0 +1,15 @@
+const UmkmSummary = () => {
+    return (
+        <div className="bg-white py-4 xl:px-0 p-4">
+            <div className="max-w-[1240px] mx-auto flex gap-4 ">
+                <img src="https://i.ibb.co/BwNSbMb/logo-mamo.jpg" alt="" className="w-[130px] rounded-full" />
+                <div>
+                    <h1 className="font-h1 text-2xl font-inter">Sambal Mamo</h1>
+                    <p className="font-inter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate! Numquam natus accusantium velit iusto sapiente quidem ullam pariatur ipsam tenetur tempore, distinctio facere at vitae ducimus, cupiditate saepe quia? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt hic corporis voluptates perspiciatis consequuntur soluta quaerat, quos non distinctio provident officiis porro magnam, praesentium velit beatae animi labore ad.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UmkmSummary;
