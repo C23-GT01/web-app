a481fe93a2f7cbaa03da286fb356cab5dd9e8b30
Abid <abidmhsn@gmail.com>
2023-12-06 22:58:12 +0800

    tambah Image Category

--- src/components/Section/Filter.jsx
+++ src/components/Section/Filter.jsx
@@ -1,12 +1,16 @@
 import FilterItem from "../Elements/FilterItem";
+import CategoryImage from "../../assets/img/categoryAll.png";
+import MakananImage from "../../assets/img/makanan.png";
+import MinumanImage from "../../assets/img/minuman.png";
+
 
 const FIlter = () => {
   return (
     <div className="w-full bg-white">
       <div className="flex gap-4 max-w-[1240px] mx-auto overflow-x-auto xl:px-0  p-4">
-        <FilterItem>Semua Kategori</FilterItem>
-        <FilterItem>Makanan</FilterItem>
-        <FilterItem>Minuman</FilterItem>
+        <FilterItem image={CategoryImage}>Semua Kategori</FilterItem>
+        <FilterItem image={MakananImage}>Makanan</FilterItem>
+        <FilterItem image={MinumanImage}>Minuman</FilterItem>
       </div>
     </div>
   );
