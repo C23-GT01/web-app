e9ac7d55318b7b5e9f90d1340a91265efe4be7a5
alfimonth <alvy1610@gmail.com>
2024-03-21 00:11:19 +0700

    fix: edit image failed
    change url from http to https

--- src/components/Section/EditUmkmSummary.jsx
+++ src/components/Section/EditUmkmSummary.jsx
@@ -92,7 +92,7 @@ const EditUmkmSummary = ({ move }) => {
             }
           };
 
-          const response = await axios.post('http://c23-gt01-01.et.r.appspot.com/upload/images', formData, config);
+          const response = await axios.post('https://c23-gt01-01.et.r.appspot.com/upload/images', formData, config);
           console.log('Server response:', response.data.data.fileLocation);
           setFileLocation(response.data.data.fileLocation);
           setFileLocationUpdated(true);
