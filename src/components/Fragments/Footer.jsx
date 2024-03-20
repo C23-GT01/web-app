2b39bf346da61dd95ba70a1fc8baeec3f8df0412
alfimonth <alvy1610@gmail.com>
2024-03-20 23:29:20 +0700

    fix: some closed text in footer
    change footer height

--- src/components/Fragments/Footer.jsx
+++ src/components/Fragments/Footer.jsx
@@ -2,7 +2,7 @@ import FooterBox from "../Elements/FooterBox";
 
 const Footer = () => {
   return (
-    <footer className="h-96 w-full bg-slate-800 mt-8">
+    <footer className="h-min w-full bg-slate-800 mt-8">
       <FooterBox/>
     </footer>
   );
